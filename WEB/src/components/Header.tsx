9c5f683a6e736d1eb02675164c9113a154466c64
Jonas Marques <jonasmarquesuitec@gmail.com>
2025-06-21 12:18:00 -0300

    feat: improve Header

--- WEB/src/components/Header.tsx
+++ WEB/src/components/Header.tsx
@@ -1,30 +1,41 @@
 "use client";
-import { Plus } from "phosphor-react";
-import { useState } from "react";
+import { Plus, X } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  function handle() {
-    setIsModalOpen(!isModalOpen);
-  }
-
   return (
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-      <h1 className="text-4xl font-bold text-violet-700 tracking-tight">
-        System
+      <h1 className="text-4xl font-bold text-violet-500 tracking-tight">
+        Activity System
       </h1>
 
-      {isModalOpen ? <h1>teste</h1> : null}
+      <Dialog.Root>
+        <Dialog.Trigger
+          type="button"
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors"
+        >
+          <Plus size={20} className="text-violet-500" />
+          Novo hábito
+        </Dialog.Trigger>
 
-      <button
-        type="button"
-        onClick={handle}
-        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors"
-      >
-        <Plus size={20} className="text-violet-500" />
-        New
-      </button>
+        <Dialog.Portal>
+          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+
+          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Close className="absolute top-6 right-6 text-zinc-400 hover:text-zinc-200 transition-colors">
+              <X size={24} aria-label="close" />
+            </Dialog.Close>
+
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+              Criar hábito
+            </Dialog.Title>
+            
+            <NewHabitForm />
+          </Dialog.Content>
+
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }

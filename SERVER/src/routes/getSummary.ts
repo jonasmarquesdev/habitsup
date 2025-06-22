56a129188da8bfbec8401144080e77c6930b6329
Jonas Marques <jonasmarquesuitec@gmail.com>
2025-06-22 08:02:23 -0300

    fix: summary route problem

--- SERVER/src/routes/getSummary.ts
+++ SERVER/src/routes/getSummary.ts
@@ -3,25 +3,32 @@ import { prisma } from "../lib/prisma";
 
 export async function getSummaryRoute(app: FastifyInstance) {
   app.get("/summary", async () => {
-    const summary = await prisma.$queryRaw`
-        SELECT
-            D.id,
-            D.date,
-            (
-            SELECT COUNT(*)::float
-            FROM day_habits DH
-            WHERE DH.day_id = D.id
-            ) AS completed,
-            (
-            SELECT COUNT(*)::float
-            FROM habit_week_days HWD
-            JOIN habits H ON H.id = HWD.habit_id
-            WHERE
-                HWD.week_day = EXTRACT(DOW FROM D.date)::int
-                AND H.created_at <= D.date
-            ) AS amount
-        FROM days D;
-    `;
+    const summary = await prisma.$queryRawUnsafe(`
+      WITH dates AS (
+        SELECT generate_series(
+          date_trunc('year', current_date)::date,
+          current_date,
+          interval '1 day'
+        ) AS date
+      )
+      SELECT
+        dates.date,
+        (
+          SELECT CAST(COUNT(*) AS FLOAT)
+          FROM day_habits dh
+          JOIN days d ON d.id = dh.day_id
+          WHERE d.date = dates.date
+        ) AS completed,
+        (
+          SELECT CAST(COUNT(*) AS FLOAT)
+          FROM habit_week_days hwd
+          JOIN habits h ON h.id = hwd.habit_id
+          WHERE hwd.week_day = EXTRACT(DOW FROM dates.date)::INT
+            AND h.created_at <= dates.date
+        ) AS amount
+      FROM dates
+      ORDER BY dates.date;
+    `);
 
     return summary;
   });

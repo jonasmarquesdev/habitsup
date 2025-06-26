94b4e680a4baad55e27dbdbc8376130bb21d7d67
Jonas Marques <jonasmarquesuitec@gmail.com>
2025-06-26 08:22:09 -0300

    fix: axios baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,5 +5,5 @@ const baseURL = process.env.NODE_ENV === 'development'
   : process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://sys-activity-api.vercel.app",
+  baseURL: baseURL || "https://sys-activity-api.vercel.app",
 });
